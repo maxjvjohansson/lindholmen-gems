6e3c119746051b4341ea3eef9581024d8415f683
maxjvjohansson <max.jv.johansson@gmail.com>
2025-08-19 11:32:16 +0200

    Add: Setup basic nextJs boilerplate

---
+++ src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+});
+
+export const metadata = {
+  title: "Lindholmen Gems",
+  description:
+    "En interaktiv skattjakt och lunchpromenad på Lindholmen. Utforska området, lös utmaningar vid olika platser och upptäck nya sidor av stadsdelen på ett roligt och aktivt sätt.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sv">
+      <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+      </head>
+      <body className={`${poppins.variable} antialiased`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
