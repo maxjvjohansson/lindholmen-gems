0d8d8f0ed54c59fd28bac33241eb6cfd7e9164f6
maxjvjohansson <max.jv.johansson@gmail.com>
2025-08-19 14:59:00 +0200

    Add: Startpage routing to explore page

--- src/app/start/page.jsx
+++ src/app/start/page.jsx
@@ -1,3 +1,19 @@
+import Button from "@/components/Button/Button";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+
 export default function StartPage() {
-  return <section></section>;
+  return (
+    <section>
+      <div className="flex flex-col gap-4 w-full max-w-sm mb-10">
+        <Button
+          href="/explore"
+          variant="primary"
+          size="lg"
+          Icon={PlayArrowIcon}
+        >
+          Start
+        </Button>
+      </div>
+    </section>
+  );
 }
