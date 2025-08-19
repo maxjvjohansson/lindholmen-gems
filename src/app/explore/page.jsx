5db8572dbe7474ffd242cb90d142b1442db8c544
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-08-19 15:43:03 +0200

    Add progressbar

--- src/app/explore/page.jsx
+++ src/app/explore/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import ProgressBar from "@/components/ProgressBar/ProgressBar";
 
 // Dynamically import to avoid SSR issues with Leaflet
 const Map = dynamic(() => import("@/components/Map/Map"), {
@@ -11,15 +12,21 @@ const Map = dynamic(() => import("@/components/Map/Map"), {
 export default function ExplorePage() {
   return (
     <section className="w-full p-0">
-      <div className="w-full max-w-2xl mx-auto px-6 mt-10">
-        <div className="w-full h-[180px]">
-          <Map
-            className="w-full h-full"
-            zoom={15}
-            minZoom={15}
-            restrictPanning
-            restrictToInitialView
-          />
+      <div
+        className="relative w-full p-0 mt-[72px]"
+        style={{ height: "calc(100vh - 72px)" }}
+      >
+        <Map
+          className="w-full h-full"
+          zoom={16}
+          minZoom={16}
+          restrictPanning
+          restrictToInitialView
+        />
+      </div>
+      <div className="fixed top-0 left-0 right-0 z-10 p-4 pointer-events-none">
+        <div className="max-w-2xl mx-auto pointer-events-auto">
+          <ProgressBar value={30} label="1/4" />
         </div>
       </div>
     </section>
