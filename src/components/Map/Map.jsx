c5e3164e239fa34ce84224cb3ba19592db17ea61
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-08-19 14:21:10 +0200

    Add map feature

---
+++ src/components/Map/Map.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useEffect } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+// Best-effort fix to ensure default marker icons load correctly in Next.js
+// by explicitly providing the asset URLs bundled by the build tool.
+import icon2x from "leaflet/dist/images/marker-icon-2x.png";
+import icon from "leaflet/dist/images/marker-icon.png";
+import shadow from "leaflet/dist/images/marker-shadow.png";
+
+export default function Map({
+  center = [57.706028, 11.936274],
+  zoom = 15,
+  minZoom = 3,
+  maxZoom = 19,
+  className,
+}) {
+  useEffect(() => {
+    L.Icon.Default.mergeOptions({
+      iconRetinaUrl: icon2x?.src || icon2x,
+      iconUrl: icon?.src || icon,
+      shadowUrl: shadow?.src || shadow,
+    });
+  }, []);
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      minZoom={minZoom}
+      maxZoom={maxZoom}
+      scrollWheelZoom={true}
+      className={className}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        maxZoom={maxZoom}
+        minZoom={minZoom}
+      />
+    </MapContainer>
+  );
+}
