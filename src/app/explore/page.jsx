a806e6faab034979d9f61f31c8ae7e8a7aaf2978
Johan-Hagman <steven.gerard.hagman@gmail.com>
2025-08-20 14:01:02 +0200

    bug fix

--- src/app/explore/page.jsx
+++ src/app/explore/page.jsx
@@ -1,6 +1,11 @@
 // app/explore/page.jsx
 import ExploreClient from "./ExploreClient";
+import { Suspense } from "react";
 
 export default function ExplorePage() {
-  return <ExploreClient />;
+  return (
+    <Suspense fallback={<div>Loading Explore...</div>}>
+      <ExploreClient />
+    </Suspense>
+  );
 }
